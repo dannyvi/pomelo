fa550c5444130ae33c66c89cc595b0a9729ff245
dannyvi <dannyvis@icloud.com>
2019-10-29 23:10:11 +0800

    icon url

--- generic/serializers.py
+++ generic/serializers.py
@@ -7,6 +7,13 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image
         fields = "__all__"
 
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        request = self.context.get('request')
+        url = ret['url']
+        ret['url'] = request.build_absolute_uri(url)
+        return ret
+
 
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
