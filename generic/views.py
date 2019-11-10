673b6b4cdae55ad866e76da00ebba0c151786387
dannyvi <dannyvis@icloud.com>
2019-11-10 17:35:10 +0800

    更新content_manage

--- generic/views.py
+++ generic/views.py
@@ -1,12 +1,24 @@
 from pomelo.decorators import swagger_viewset
 from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
-
+from django.utils.translation import gettext_lazy as _
 from .models import Image, Video
 from .serializers import ImageSerializer, VideoSerializer
+from drf_yasg import openapi
+from django.utils.decorators import method_decorator as md
+from drf_yasg.utils import swagger_auto_schema as sas
+from rest_framework.decorators import parser_classes
+from rest_framework.parsers import MultiPartParser
 
 
-@swagger_viewset('图片管理', '图片管理', ['pomelo-常用功能-图片管理'])
+@md(name='create',
+    decorator=sas(
+        manual_parameters=[openapi.Parameter('url',
+                                             openapi.IN_FORM,
+                                             description=_('Upload image.'),
+                                             type=openapi.TYPE_FILE), ],
+        tags=['图片上传']))
+@parser_classes([MultiPartParser, ])
 class ImageViewSet(ModelViewSet):
     """Image Management Viewset."""
     queryset = Image.objects.all()
@@ -14,7 +26,15 @@ class ImageViewSet(ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
-@swagger_viewset('视频管理', '视频管理', ['pomelo-常用功能-视频管理'])
+
+@md(name='create',
+    decorator=sas(
+        manual_parameters=[openapi.Parameter('url',
+                                             openapi.IN_FORM,
+                                             description='上传视频',
+                                             type=openapi.TYPE_FILE), ],
+        tags=['视频上传']))
+@parser_classes([MultiPartParser, ])
 class VideoViewSet(ModelViewSet):
     """Image Management Viewset."""
     queryset = Video.objects.all()
