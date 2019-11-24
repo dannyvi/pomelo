a052ff6d992ce99eceaef5281e48e01605df5af5
dannyvi <dannyvis@icloud.com>
2019-11-24 16:30:35 +0800

    videos for product

--- generic/serializers.py
+++ generic/serializers.py
@@ -19,3 +19,10 @@ class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
         fields = "__all__"
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        request = self.context.get('request')
+        url = ret['url']
+        ret['url'] = request.build_absolute_uri(url)
+        return ret
